ebeba867d6759e59ea9bb93d178ec68180a91c17
Logan Martlew <logan.martlew@gmail.com>
2023-01-23 19:34:37 +1300

    Added items list

--- src/pages/ItemsPage.tsx
+++ src/pages/ItemsPage.tsx
@@ -4,15 +4,18 @@ import {
   ScrollArea,
   Select,
   Stack,
+  Text,
   TextInput,
   Title,
 } from '@mantine/core';
 import { FC } from 'react';
 import { MdAdd, MdSearch } from 'react-icons/md';
 import { useHousehold } from '~/features/household/useHousehold';
+import { useItem } from '~/features/items/useItem';
 
 const ItemsPage: FC = () => {
   const { currentHousehold } = useHousehold();
+  const { filteredItems } = useItem();
 
   return (
     <Box p='md'>
@@ -24,7 +27,15 @@ const ItemsPage: FC = () => {
         <Stack>
           <TextInput placeholder='Search items' rightSection={<MdSearch />} />
         </Stack>
-        <ScrollArea></ScrollArea>
+        <ScrollArea>
+          <Stack>
+            {filteredItems.length < 1 && (
+              <Text>No items in this household...</Text>
+            )}
+            {filteredItems.length >= 1 &&
+              filteredItems.map(item => <p>{item.name}</p>)}
+          </Stack>
+        </ScrollArea>
       </Stack>
       <Box p='md' sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}>
         <Button
