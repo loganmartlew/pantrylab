137f90d4c979b74dde099a41df114d8aae83eb68
Logan Martlew <logan.martlew@gmail.com>
2023-05-22 17:55:05 +1200

    Added next-pwa

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig);
