24d48aefa50f2af81956a27dba2f626e1b22ec37
Logan Martlew <logan.martlew@gmail.com>
2023-06-10 20:21:28 +1200

    Initialised prisma inside nestjs

---
+++ prisma/migrations/20230610080946_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Household" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Household_pkey" PRIMARY KEY ("id")
+);
