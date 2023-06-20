2a2b43c2bf727d5617b11bc9e9f0d358d2db2d4c
Logan Martlew <logan.martlew@gmail.com>
2023-06-20 17:10:29 +1200

    Created service method for signing up

--- apps/server/src/auth/auth.service.ts
+++ apps/server/src/auth/auth.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { DbService } from '../db/db.service';
+import { SignupDto } from './dto/signup.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private db: DbService) {}
+
+  async signup(signupDto: SignupDto) {
+    const passwordHash = await this.hashPassword(signupDto.password);
+
+    const user = await this.db.user.create({
+      data: {
+        email: signupDto.email,
+        firstName: signupDto.firstName,
+        lastName: signupDto.lastName,
+        passwordHash,
+      },
+    });
+  }
+
+  hashPassword(password: string) {
+    return bcrypt.hash(password, 10);
+  }
+}
